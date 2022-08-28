e6b98018c5b76edd6e28d6d3a351a787c453f1e0
shivam-singh-cse <shivanshsingh9358@gmail.com>
2022-08-28 23:23:39 +0530

    Updates

---
+++ precache-manifest.fd76ef77916cd870998b954e55810dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e94b8975cedc519b72",
+    "url": "/reactcalculator/static/css/main.41eb617d.chunk.css"
+  },
+  {
+    "revision": "26e94b8975cedc519b72",
+    "url": "/reactcalculator/static/js/main.26e94b89.chunk.js"
+  },
+  {
+    "revision": "2e66af350e672a7ffdd6",
+    "url": "/reactcalculator/static/js/1.2e66af35.chunk.js"
+  },
+  {
+    "revision": "1da360c10946cb10d78f",
+    "url": "/reactcalculator/static/js/runtime~main.1da360c1.js"
+  },
+  {
+    "revision": "932b64dbecbe0a5dfcf581333c408d76",
+    "url": "/reactcalculator/index.html"
+  }
+];
